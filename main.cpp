03d1880936c9591dea4b1f1920312154d8051089
Markaryan Arthur <armarkaryan-promo@yandex.ru>
2026-05-01 12:40:22 +0300

    style: add doxygen-format file description

--- main.cpp
+++ main.cpp
@@ -1,3 +1,36 @@
+/**
+ * @file        main.cpp
+ * @brief       Program entry point for uBak application.
+ * @details     This file contains the main() function which:
+ *              - Creates a QApplication instance
+ *              - Loads and installs system-appropriate language translations
+ *              - Creates and displays the main application window
+ *              - Starts the Qt event loop
+ *
+ * @author      Arthur Markaryan
+ * @date        01.05.2026
+ * @version     1.0
+ * @license     LGPL v3.0
+ * @copyright   Copyright (c) 2026
+ *
+ * @par Dependencies:
+ * - Qt5/6 Core (QApplication, QLocale, QTranslator)
+ * - MainWindow class
+ *
+ * @par Translation files:
+ * Translation files are stored in the :/i18n/ resource path
+ * with naming pattern: "uBak_<locale>.qm"
+ *
+ * @par ChangeLog:
+ * 01.05.2026   v1.0    Arthur Markaryan - Initial implementation
+ *
+ * @see         QApplication::exec()
+ * @see         QTranslator::load()
+ * @see         MainWindow::show()
+ *
+ * @return      int - Application exit code (0 for success)
+ */
+
 #include "mainwindow.h"
 
 #include <QApplication>
--- mainwindow.h
+++ mainwindow.h
@@ -1,3 +1,20 @@
+/**
+ * @file		mainwindow.h
+ * @brief		Main window module (header file).
+ * @details		Contains the definition of the MainWindow class for Qt GUI application.
+ * @note		...
+ * @author		Arthur Markaryan
+ * @date		01.05.2026
+ * @version		1.0
+ * @license     LGPL v3.0
+ * @copyright   Copyright (c) 2026
+ *
+ * @par ChangeLog:
+ * 01.05.2026	v1.0	Base creation
+ *
+ * @par ToDo: ...
+ */
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
